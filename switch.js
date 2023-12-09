41f20438e90c96729c88916d5cba2db3e5584fa1
Mikayil <mmigeleo221002@gmail.com>
2023-12-09 20:57:33 +0400

    added outcome

--- switch.js
+++ switch.js
@@ -4,22 +4,22 @@ let outcome;
 
 switch (login, password) {
     case (login = 'admin') && (password = '123456'):
-        netice = 'Salam Admin! Sayti redakte ede bilersiniz.'
+        outcome = 'Salam Admin! Sayti redakte ede bilersiniz.'
         break;
     case (login = 'ayaz') && (password = '123456'):
-        netice = 'Xos geldin, Ayaz!'
+        outcome = 'Xos geldin, Ayaz!'
         break;
     case (login = 'murad') && (password = 'murik1234'):
-        netice = 'Xos geldin, Murad!'
+        outcome = 'Xos geldin, Murad!'
         break;
     case  (login = 'mika') && (password = 'mika123'):
-        netice = 'Xos geldin, Mikayil!'
+        outcome = 'Xos geldin, Mikayil!'
         break;
     case (login = null) && (password = null):
-        netice = 'Get admin ve ya parol yaz, zeif.'
+        outcome = 'Get admin ve ya parol yaz, zeif.'
         break;
     default:
-        netice = 'Login ve ya parol sehvdir.'
+        outcome = 'Login ve ya parol sehvdir.'
     break;
 }
 
